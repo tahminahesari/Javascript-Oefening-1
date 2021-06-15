1d6dad702d72da5f108f921b583477cd5403477b
tahminahesari <hesari@live.com>
2021-06-15 23:54:16 +0200

    'variabele'

--- variables.js
+++ variables.js
@@ -14,33 +14,28 @@ console.log(name);
 const eyeColor = "Brown";
 console.log(eyeColor);
 
-
-
-// ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
-// ==========================================
+let age = 34;
+console.log(age);
 
-
-// ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
-// ==========================================
+let profession = "Student";
+console.log(profession);
 
 
-// ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
-// ==========================================
+const shoeSize = 37;
+console.log(shoeSize);
 
 
-// ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
-// ==========================================
+let  currentState = "Happy with my man"
+console.log(currentState);
 
-
-// ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
-// ==========================================
+let children = 0;
+console.log(children);
 
 
-// ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
-// ==========================================
+
