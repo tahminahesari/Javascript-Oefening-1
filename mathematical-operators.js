8d9c993af43ab76660575dc95e1ffce66f44276c
tahminahesari <hesari@live.com>
2021-06-16 00:29:57 +0200

    einde opdracht

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -2,39 +2,46 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
-// ==========================================
+const plus = 4 + 5;
+console.log(plus);
 
 
-// ==========================================
+
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
-// ==========================================
+const multiply = 7 * 52;
+console.log(multiply);
 
 
-// ==========================================
+
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
-// ==========================================
+const multiplyPlus = 4 + 3;
+console.log(multiplyPlus * 7);
 
 
-// ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
-// ==========================================
+let divide = 36 / 6;
+console.log(divide * 2 );
 
-
-// ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
-// ==========================================
+let sweet = "zoet";
+let juicy = "sappig"
+console.log(sweet + juicy);
 
 
-// ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
+const fistName = "Tahmina ";
+const lastName = "Hesari";
+console.log(fistName + lastName);
 
 
-// ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
-// ==========================================
+const place = "Udenhout ";
+const province = "Noord Brabant";
+console.log(place + province);
+
